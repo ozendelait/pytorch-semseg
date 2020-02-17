-fixed wrong dimensions in frrn loader

ptsemseg/loader/mapillary_vistas_loader.py
@@ -44,22 +44,27 @@ class mapillaryVistasLoader(data.Dataset):
         self.mean = np.array([80.5423, 91.3162, 81.4312])
         self.files = {}
 
-        self.images_base = os.path.join(self.root, self.split, "images")
-        self.annotations_base = os.path.join(self.root, self.split, "labels")
         self.boost_idx_per_frm = {}
-
-        if not frame_list is None:
-            frame_list_jpgs = []
-            for (lbl_path, boost_idx) in frame_list:
-                frm_path = os.path.join(self.images_base, os.path.basename(lbl_path).replace(".png", ".jpg"))
-                frame_list_jpgs.append(frm_path)
-                self.boost_idx_per_frm[frm_path] = boost_idx
-            self.files[split] = frame_list_jpgs
+        if test_mode:
+            pass # dont load any files via fileloader
         else:
-            self.files[split] = recursive_glob(rootdir=self.images_base, suffix=".jpg")
+            self.images_base = os.path.join(self.root, self.split, "images")
+            self.annotations_base = os.path.join(self.root, self.split, "labels")
+            if not frame_list is None:
+                frame_list_jpgs = []
+                for (lbl_path, boost_idx) in frame_list:
+                    frm_path = os.path.join(self.images_base, os.path.basename(lbl_path).replace(".png", ".jpg"))
+                    frame_list_jpgs.append(frm_path)
+                    self.boost_idx_per_frm[frm_path] = boost_idx
+                self.files[split] = frame_list_jpgs
+            else:
+                self.files[split] = recursive_glob(rootdir=self.images_base, suffix=".jpg")
         
 
-        self.class_ids, self.class_names, self.class_colors = self.parse_config()
+            self.class_ids, self.class_names, self.class_colors = self.parse_config()
+            if not self.files[split]:
+                raise Exception("No files for split=[%s] found in %s" % (split, self.images_base))
+            print("Found %d %s images" % (len(self.files[split]), split))
 
         self.ignore_id = 250
         self.first_run = True
@@ -87,10 +92,6 @@ class mapillaryVistasLoader(data.Dataset):
         250, 250, 250, 250, 250, 250, 250, 250, 250]
             self.lut = np.array(map_to_cs).astype(np.uint8)
             self.n_classes = 19
-        if not self.files[split]:
-            raise Exception("No files for split=[%s] found in %s" % (split, self.images_base))
-
-        print("Found %d %s images" % (len(self.files[split]), split))
 
     def parse_config(self):
         with open(os.path.join(self.root, "config.json")) as config_file:

ptsemseg/models/frrn.py
@@ -25,8 +25,8 @@ class frrn(nn.Module):
     1) Original Author's code: https://github.com/TobyPDE/FRRN
     2) TF implementation by @kiwonjoon: https://github.com/hiwonjoon/tf-frrn
     """
-
-    def __init__(self, n_classes=21, model_type="B", group_norm=False, n_groups=16):
+#ignore input_size for now
+    def __init__(self, n_classes=21, model_type="B", group_norm=False, n_groups=16, input_size=None):
         super(frrn, self).__init__()
         self.n_classes = n_classes
         self.model_type = model_type
@@ -137,7 +137,9 @@ class frrn(nn.Module):
             # pass through encoding FRRUs
             for block in range(n_blocks):
                 key = "_".join(map(str, ["encoding_frru", n_blocks, channels, scale, block]))
+                #print("Dec-Incoming FRRU Size: ", y_pooled, y.shape, z.shape)
                 y, z = getattr(self, key)(y_pooled, z)
+                #print("Dec-Outgoing FRRU Size: ",  y.shape, z.shape)
             prev_channels = channels
 
         # decoding
@@ -148,9 +150,9 @@ class frrn(nn.Module):
             # pass through decoding FRRUs
             for block in range(n_blocks):
                 key = "_".join(map(str, ["decoding_frru", n_blocks, channels, scale, block]))
-                # print("Incoming FRRU Size: ", key, y_upsampled.shape, z.shape)
+                #print("Incoming FRRU Size: ", key, upsample_size, y_upsampled.shape, z.shape)
                 y, z = getattr(self, key)(y_upsampled, z)
-                # print("Outgoing FRRU Size: ", key, y.shape, z.shape)
+                #print("Outgoing FRRU Size: ", key, y.shape, z.shape)
             prev_channels = channels
 
         # merge streams

ptsemseg/models/utils.py
@@ -395,14 +395,17 @@ class FRRU(nn.Module):
         self.conv_res = nn.Conv2d(out_channels, 32, kernel_size=1, stride=1, padding=0)
 
     def forward(self, y, z):
+       # print("SIZES0: ", y.shape, z.shape)
         x = torch.cat([y, nn.MaxPool2d(self.scale, self.scale)(z)], dim=1)
         y_prime = self.conv1(x)
+        #print("SIZES1: ", x.shape, y_prime.shape)
         y_prime = self.conv2(y_prime)
-
         x = self.conv_res(y_prime)
-        upsample_size = torch.Size([_s * self.scale for _s in y_prime.shape[-2:]])
-        x = F.interpolate(x, size=upsample_size, mode="nearest", align_corners=False)
-        z_prime = z + x
+        #print("SIZES2: ", x.shape, y_prime.shape)
+        upsample_size = [_s * self.scale for _s in y_prime.shape[-2:]]
+        x = F.interpolate(x, size=upsample_size, mode="nearest", align_corners=None)
+        #print("SIZES3: ", upsample_size, z.shape, x.shape)
+        z_prime = z[:,:,:x.shape[2],:x.shape[3]] + x
 
         return y_prime, z_prime
 

test_callable.py
@@ -19,10 +19,16 @@ def files_in_subdirs(start_dir, pattern = ["*.png","*.jpg","*.jpeg"]):
 mean_rgb = {
         "pascal": [103.939, 116.779, 123.68],
         "cityscapes": [0.0, 0.0, 0.0],
-        "railsem19": [0.0, 0.0, 0.0]}
+        "railsem19": [0.0, 0.0, 0.0],
+        "vistas": [80.5423, 91.3162, 81.4312]}
 
 def prepare_img(img0, orig_size, img_mean, img_norm):
-    img = misc.imresize(img0, orig_size)  # uint8 with RGB mode
+    if img0.shape[0] < orig_size[0] and img0.shape[1] < orig_size[1]: #apply padding, keep image in center
+        w_add_both = orig_size[1]-img0.shape[1]
+        h_add_both = orig_size[0]-img0.shape[0]
+        img = np.pad(img0,pad_width=[(h_add_both//2,h_add_both-h_add_both//2),(w_add_both//2,w_add_both-w_add_both//2),(0,0)],mode='constant', constant_values=0)
+    else:
+        img = misc.imresize(img0, orig_size)  # uint8 with RGB mode
     img = img[:, :, ::-1]  # RGB -> BGR
     img = img.astype(np.float64)
     img -= img_mean
@@ -46,10 +52,11 @@ def test(args):
     print("Read Input Image from : {}".format(args.img_path))
 
     if args.inp_dim == None:
-       img = misc.imread(allfiles[0])
-       orig_size = img.shape[:-1]
+        img = misc.imread(allfiles[0])
+        orig_size = img.shape[:-1]
     else:
-       orig_size = [int(dim) for dim in args.inp_dim.split("x")]
+        orig_size = [int(dim) for dim in args.inp_dim.split("x")]
+        orig_size = [orig_size[1],orig_size[0]]
 
     print(orig_size)
     data_loader = get_loader(args.dataset)